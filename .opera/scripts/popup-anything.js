c45e4eca35bbae9d01be52accc3214821c3d0fe6
emonkak <emonkak@gmail.com>
2010-06-16 17:33:51 +0900

    opera: Update popup-anything.js

--- .opera/scripts/popup-anything.js
+++ .opera/scripts/popup-anything.js
@@ -8,7 +8,7 @@
 // ==/UserScript==
 
 (function() {
-  const IFRAME_WINDOW_NAME = 'IFRAME_HTTP_REQUEST';
+  const IFRAME_WINDOW_NAME = 'POPUP_ANYTHING';
   const LOADING_IMAGE = 'data:image/gif;base64,' +
     'R0lGODlhGAAYAPYAAAAAAAUFBQoKCg0NDRMTExwcHCAgICcnJykpKSsrKzMzMzY2Nj09PUJCQkdH' +
     'R1JSUlRUVGpqam5ubnt7e319fY+Pj5CQkKGhoaSkpMnJyczMzAQEBAkJCQ4ODhAQEBUVFRkZGR4e' +
@@ -95,7 +95,7 @@
     },
     '#popup-anything ul': {
       'display': 'block',
-      'list-style': 'square none outside',
+      'list-style': 'disc none outside',
       'margin': 0,
       'padding': 0,
     },
@@ -121,7 +121,7 @@
       method: 'GET',
       url: 'http://eow.alc.co.jp/%s/UTF-8/',
       pattern: /^ *[a-zA-Z-]+ *$/,
-      xpath: '//div[@id="resultList"]/ul',
+      xpath: '//div[@id="resultsList"]/ul',
     },
     {
       method: 'GET',
@@ -134,7 +134,7 @@
       method: 'GET',
       url: 'http://kotobank.jp/word/%s',
       pattern: /^ *[\u2e00-\uffff]+ *$/,
-      xpath: 'descendant::div[@class="full"][1]/p[@class="doc"]',
+      xpath: '//div[@class="wordTitle"][1]/h1|descendant::div[@class="full"]/p[@class="doc"]',
     },
     {
       method: 'POST',
@@ -170,10 +170,10 @@
   ];
 
   if (window.parent === window) {
-    add_style_sheet(POPUP_STYLE);
+    addStyle(POPUP_STYLE);
 
     document.addEventListener('DOMContentLoaded', function(e){
-      var popup = init_interface();
+      var popup = initPopup();
 
       document.addEventListener('mouseup', function(e){
         var siteinfo = e.altKey  ? SITEINFO_ALT :
@@ -182,25 +182,26 @@
           return;
 
         var query = document.getSelection();
-        if (!query || new RegExp(/^\s*$/).test(query))
+        if (!query || /^\s*$/.test(query))
           return;
 
         var params = [];
-        for (var i = 0, l = siteinfo.length; i < l; i++)
-          if (new RegExp(siteinfo[i].pattern).test(query))
+        for (var i = 0, l = siteinfo.length; i < l; i++) {
+          if (siteinfo[i].pattern.test(query))
             params.push(siteinfo[i]);
+        };
         if (params.length === 0)
           return;
 
         popup.load();
-        iframe_http_reqest(params.shift(), query, function(e){
+        iFrameHttpReqest(params.shift(), query, function(e){
           if (!popup.disp())
             return;
 
           if (e.data) {
             popup.apply(e.data);
           } else if (params.length > 0) {
-            iframe_http_reqest(params.shift(), query, arguments.callee);
+            iFrameHttpReqest(params.shift(), query, arguments.callee);
           } else {
             popup.hide();
           }
@@ -208,7 +209,7 @@
       }, false);
     }, false);
   } else if (window.name.indexOf(IFRAME_WINDOW_NAME) === 0) {
-    add_style_sheet({'body': {'display': 'none'}});
+    addStyle({'body': {'display': 'none'}});
 
     window.opera.addEventListener('BeforeScript', function(e){
       e.preventDefault();
@@ -217,35 +218,41 @@
       e.preventDefault();
     }, false);
     window.opera.addEventListener('BeforeEventListener', function(e){
-      if (e.event.type != 'DOMContentLoaded')
+      if (e.event.type !== 'DOMContentLoaded')
         e.preventDefault();
     }, false);
 
     document.addEventListener('DOMContentLoaded', function(e){
-      var xpath = window.name.split('\n')[1];
-      var entry = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-      var contents = [];
+      if (typeof(JSON) !== 'object') {
+        var JSON = {};
+        JSON.parse = function(value){
+          return eval('(' + value + ')');
+        };
+      }
+      var json = JSON.parse(window.name.slice(IFRAME_WINDOW_NAME.length));
+      var entry = document.evaluate(json.xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
+      var contents = '';
       for (var i = 0, l = entry.snapshotLength; i < l; i++) {
         var node = entry.snapshotItem(i);
-        contents.push(parse_node(node));
+        contents += parseNode(node);
       }
 
-      window.parent.postMessage(contents.join(''), document.referrer);
+      window.parent.postMessage(contents, json.referrer);
     }, false);
   }
 
-  function parse_node(node) {
+  function parseNode(node) {
     if (node.nodeType != Node.ELEMENT_NODE && node.nodeType != Node.TEXT_NODE)
       return;
 
     var children = node.childNodes;
-    var contents = [];
+    var contents = '';
     for (var i = 0, l = children.length; i < l; i++) {
-      var content = arguments.callee(children[i]);
-      if (content)
-        contents.push(content);
-    }
+      var childContents = arguments.callee(children[i]);
+      if (childContents)
+        contents += childContents;
+    };
 
     switch (node.nodeType) {
     case Node.ELEMENT_NODE:
@@ -268,7 +275,7 @@
         tag = 'strong';
         break;
       case 'span':
-        // Delete ruby for iknow dictionary.
+        // Delete ruby for alc dictionary.
         if (node.className === 'kana')
           return;
         break;
@@ -278,14 +285,14 @@
         return;
       }
       return children.length === 0
-           ? '<' + tag + attr.join(' ') + '/>' + contents.join('')
-           : '<' + tag + attr.join(' ') + '>'  + contents.join('') + '</' + tag + '>';
+           ? '<' + tag + attr.join(' ') + '/>' + contents
+           : '<' + tag + attr.join(' ') + '>'  + contents + '</' + tag + '>';
     case Node.TEXT_NODE:
       return node.nodeValue;
     }
   }
 
-  function init_interface()
+  function initPopup()
   {
     var Popup = function(){
       this.node = document.createElement('div');
@@ -298,7 +305,7 @@
       }, false);
 
       document.body.appendChild(this.node);
-    }
+    };
 
     Popup.prototype.load = function(){
       var img = document.createElement('img');
@@ -313,60 +320,82 @@
       this.node.appendChild(text);
 
       this.node.style.display = 'inline';
-    }
+    };
 
     Popup.prototype.disp = function(){
       return this.node.style.display === 'none' ? false : true;
-    }
+    };
 
     Popup.prototype.apply = function(contents){
       this.node.innerHTML = contents;
-    }
+    };
 
     Popup.prototype.hide = function(){
       this.node.style.display = 'none';
       this.node.innerHTML = '';
-    }
+    };
 
     return new Popup();
   }
 
-  function add_style_sheet(css)
+  function addStyle(css)
   {
-    var rules = [];
+    var rules = '';
     for (var selector in css) {
-      var tmp = [];
-      tmp.push(selector + '{');
+      rules += selector + '{';
       for (var prop in css[selector])
-        tmp.push(prop + ':' + css[selector][prop] + ';');
-      tmp.push('}');
-      rules.push(tmp.join(''));
+        rules += prop + ':' + css[selector][prop] + ';';
+      rules += '}';
     }
 
+    var style = document.createElement("style");
+    style.type = 'text/css';
+    style.innerHTML = rules;
+
     if (document.getElementsByTagName('head').length > 0) {
-      var style = document.createElement("style");
-      style.type = 'text/css';
-      rules.forEach(function(rule){
-        style.sheet.insertRule(rule, style.sheet.cssRules.length);
-      });
-      document.getElementsByTagName("head")[0].appendChild(style)
+      document.getElementsByTagName('head')[0].appendChild(style);
     } else {
-      document.write('<style type="text/css">' + rules.join('') + '</style>');
+      document.documentElement.appendChild(style);
     }
   }
 
-  function iframe_http_reqest(param, query, callback)
+  function iFrameHttpReqest(param, query, callback)
   {
+    if (typeof(JSON) !== 'object') {
+      var JSON = {};
+      JSON.stringify = function(value){
+        switch (typeof(value)) {
+        case 'number':
+        case 'boolean':
+          return value;
+        case 'string':
+          return '"' + value.replace(/(['"])/g, '\\$1') + '"';
+        case 'object':
+          var data = [];
+          if (value instanceof Array) {
+            for (var i = 0, l = value.length; i < l; i++)
+              data.push(arguments.callee(value[i]));
+            return '[' + data.join(',') + ']';
+          } else {
+            for (var key in value)
+              data.push('"' + key + '":' + arguments.callee(value[key]));
+            return '{' + data.join(',') + '}';
+          }
+        }
+      };
+    }
+    var json = JSON.stringify({referrer: location.href, xpath: param.xpath});
+
     var iframe = document.createElement('iframe');
-    iframe.name = IFRAME_WINDOW_NAME + '\n' + param.xpath;
+    iframe.name = IFRAME_WINDOW_NAME + json;
     iframe.style.height = 0;
     iframe.style.width = 0;
     iframe.style.visibility = 'hidden';
 
     var form = document.createElement('form');
-    form.acceptCharset = param.charset ? param.charset : 'utf-8';
+    form.acceptCharset = param.charset || 'utf-8';
     form.action = param.url.replace(/%s/, query);
-    form.method = param.method;
+    form.method = param.method || 'GET';
     form.target = iframe.name;
 
     for (var key in param.data) {
