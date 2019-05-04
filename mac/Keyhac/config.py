7a3c23f513a6bd1656e3e2f5e544d0c8a99d560a
Shota Nozaki <emonkak@gmail.com>
2019-05-04 16:21:52 +0900

    Update mac relatedd files

---
+++ mac/Keyhac/config.py
@@ -0,0 +1,26 @@
+﻿from keyhac import *
+
+def configure(keymap):
+    # Font
+    keymap.setFont('Osaka-Mono', 16)
+
+    # Theme
+    keymap.setTheme('black')
+
+    # Right Alt to Right Ctrl
+    keymap.replaceKey('RAlt', 'RCtrl')
+
+    # Global keymap which affects any windows
+    keymap_global = keymap.defineWindowKeymap()
+
+    # Swap Semicolon and Colon
+    keymap_global['Shift-Semicolon'] = 'Colon'
+    keymap_global['Semicolon'] = 'Shift-Semicolon'
+
+    # VI coordinated escape
+    keymap_global['Ctrl-OpenBracket'] = '({})'.format(VK_Eisu), 'Escape'
+    keymap_global['Escape'] = '({})'.format(VK_Eisu), 'Escape'
+
+    # IME control like JIS keyboard
+    keymap_global["O-LCmd"] = '({})'.format(VK_Eisu), 'Escape'
+    keymap_global["O-RCmd"] = '({})'.format(VK_Kana)
