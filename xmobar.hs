8e72899ea27f7f8a6b3cfc0e042e6f9c497609d8
Shota Nozaki <emonkak@gmail.com>
2022-05-17 20:32:17 +0900

    Add `iconRoot` config

--- xmobar.hs
+++ xmobar.hs
@@ -2,15 +2,17 @@ import Xmobar
 
 import Monitors.CatNum (CatNum(..))
 import Monitors.PulseAudio (PulseAudio(..))
+import System.Directory (getHomeDirectory)
+import System.IO (FilePath)
 
-config :: Config
-config = defaultConfig
-  { font = "xft:Monospace:pixelsize=11,Tazugane Gothic StdN:pixelsize=11,Noto Emoji:pixelsize=11"
+makeConfig :: FilePath -> Config
+makeConfig homeDirectory = defaultConfig
+  { font = "xft:M PLUS Code Latin 60:pixelsize=11,Noto Sans CJK JP:pixelsize=11,Noto Emoji:pixelsize=11"
   , additionalFonts = [ "xft:Material Icons:pixelsize=16" ]
   , bgColor = "#21272b"
   , fgColor = "#f5f6f7"
   , position = Top
-  , overrideRedirect = False
+  , overrideRedirect = True
   , commands = [ Run StdinReader
                , Run $ MPD
                  [ "-t", "<statei> <title><fc=#869096>/</fc><artist> <fc=#869096>(<volume>%)</fc>"
@@ -33,33 +35,7 @@ config = defaultConfig
                  , "-p", "3"
                  ] 10
                , Run $ CatNum "cputemp"
-                 [ "/sys/bus/platform/devices/coretemp.0/hwmon/hwmon2/temp1_input"
-                 ]
-                 [ "-t", "<fc=#869096>(</fc><n0><fc=#869096>)</fc>"
-                 , "-L", "50000"
-                 , "-H", "70000"
-                 , "-l", "#869096"
-                 , "-n", "#5ebaf7"
-                 , "-h", "#fc8e88"
-                 , "-m", "4"
-                 , "--"
-                 , "--divier", "1000"
-                 , "--suffix", "°C"
-                 ] 10
-               , Run $ CatNum "gpu"
-                 [ "/sys/class/drm/card0/device/gpu_busy_percent"
-                 ]
-                 [ "-t", "<fc=#5ebaf7><fn=1>\xe30c</fn></fc> <n0>"
-                 , "-L", "50"
-                 , "-H", "75"
-                 , "-n", "#5ebaf7"
-                 , "-h", "#fc8e88"
-                 , "-m", "4"
-                 , "--"
-                 , "--suffix", "%"
-                 ] 10
-               , Run $ CatNum "gputemp"
-                 [ "/sys/class/drm/card0/device/hwmon/hwmon1/temp2_input"
+                 [ "/sys/bus/platform/devices/coretemp.0/hwmon/hwmon1/temp1_input"
                  ]
                  [ "-t", "<fc=#869096>(</fc><n0><fc=#869096>)</fc>"
                  , "-L", "50000"
@@ -92,8 +68,8 @@ config = defaultConfig
                  ] 100
                , Run $ Network "eth0"
                  [ "-t", "<fc=#5ebaf7><fn=1>\xe5d8</fn></fc> <tx> <fc=#5ebaf7><fn=1>\xe5db</fn></fc> <rx>"
-                 , "-L", "1000"
-                 , "-H", "100000"
+                 , "-L", "10000"
+                 , "-H", "1000000"
                  , "-l", "#869096"
                  , "-n", "#5ebaf7"
                  , "-h", "#fc8e88"
@@ -108,10 +84,14 @@ config = defaultConfig
                  , "--on", "<fc=#5ebaf7><fn=1>\xe050</fn></fc>"
                  , "--off", "<fc=#869096><fn=1>\xe04f</fn></fc>"
                  ]
-               , Run $ Date "%Y/%m/%d %a %H:%M:%S" "date" 10
+               , Run $ Date "%Y-%m-%d %a %H:%M:%S" "date" 10
                ]
-  , template = " %StdinReader% }{ %mpd%  %cpu% %cputemp%  %gpu% %gputemp%  %memory%  %disku%  %eth0%  %pulseaudio%  %date% "
+  , template = " %StdinReader% }{ %mpd%  %cpu% %cputemp%  %memory%  %disku%  %eth0%  %pulseaudio%  %date% "
+  , iconRoot = homeDirectory <> "/.xmonad/icons"
   }
 
 main :: IO ()
-main = xmobar config
+main = do
+  homeDirectory <- getHomeDirectory
+  let config = makeConfig homeDirectory
+  xmobar config
