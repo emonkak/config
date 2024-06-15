8d3111835da57995cc538d1f34b3ef0d42feebd1
Shota Nozaki <emonkak@gmail.com>
2024-06-16 00:44:01 +0900

    vim.plugin.lsp_config: fix 'signcolumn' setting (error is occurred in v0.10)

--- config/nvim/plugin/lsp_config.lua
+++ config/nvim/plugin/lsp_config.lua
@@ -143,7 +143,7 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
   callback = function(args)
     local clients = vim.lsp.get_active_clients({ bufnr = args.buf })
     if #clients > 0 then
-      vim.api.nvim_set_option_value('signcolumn', 'yes', { buf = args.buf })
+      vim.api.nvim_set_option_value('signcolumn', 'yes', { scope = 'local' })
     end
   end
 })
@@ -176,7 +176,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
       run_command_without_errors('cc')
     end)
 
-    vim.api.nvim_set_option_value('signcolumn', 'yes', { buf = args.buf })
+    vim.api.nvim_set_option_value('signcolumn', 'yes', { scope = 'local' })
 
     if client.server_capabilities.completionProvider then
       vim.bo[args.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
@@ -257,7 +257,7 @@ vim.api.nvim_create_autocmd('LspDetach', {
     unmap('<LocalLeader><LocalLeader>')
 
     vim.api.nvim_set_option_value('signcolumn', vim.o.signcolumn, {
-      buf = args.buf,
+      scope = 'local',
     })
     vim.api.nvim_set_option_value('omnifunc', nil, { buf = args.buf })
     vim.api.nvim_set_option_value('tagfunc', nil, { buf = args.buf })
