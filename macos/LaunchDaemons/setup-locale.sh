a757e0a19ce508493181e394a70b3f559215d59a
Shota Nozaki <emonkak@gmail.com>
2024-06-25 16:38:32 +0900

    macos/LaunchDaemons/setup-locale: lookup user name from UID

--- macos/LaunchDaemons/setup-locale.sh
+++ macos/LaunchDaemons/setup-locale.sh
@@ -12,20 +12,15 @@ RETRY_INTERVAL=1
 MAX_RETRIES=10
 
 retry_count=0
-domain_created=0
 
 while [[ ${retry_count} -lt ${MAX_RETRIES} ]]
 do
   if launchctl print "gui/$TARGET_UID" > /dev/null
   then
-    domain_created=1
+    user_name="$(id --name --user ${TARGET_UID})"
+    launchctl asuser ${TARGET_UID} launchctl setenv PATH_LOCALE "/Users/${user_name}/.locale"
     break
   fi
   sleep ${RETRY_INTERVAL}
   retry_count=$(( ${retry_count} + 1 ))
 done
-
-if [[ ${domain_created} -eq 1 ]]
-then
-  launchctl asuser ${TARGET_UID} launchctl setenv PATH_LOCALE /Users/emonkak/.locale
-fi
