d0b21914d8b57b88cb8b288e37c26cd1b6a94339
Shota Nozaki <emonkak@gmail.com>
2025-08-17 21:38:03 +0900

    vim.queries: enable folds at `arrow_function`, `function_expression` or `generator_function`

--- vim/queries/typescript/folds.scm
+++ vim/queries/typescript/folds.scm
@@ -1,8 +1,11 @@
 [
+  (arrow_function)
   (abstract_class_declaration)
   (class_declaration)
   (enum_declaration)
   (function_declaration)
+  (function_expression)
+  (generator_function)
   (generator_function_declaration)
   (interface_declaration)
   (internal_module)
